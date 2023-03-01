77f42d7f892ec23c7b49e90d54f7e28314ebe1f6
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-03-01 11:16:14 +0600

    doen

--- Find Country/js/catagori.js
+++ Find Country/js/catagori.js
@@ -1,1 +1,21 @@
 
+// fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+// .then(res  => res.json())
+// .then(date => console.log(date.data[0].brand))
+
+
+// let loadDate = async() => {
+//     const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+//     const res = await fetch(url);
+//     const date = await res.json();
+//     display(date.data)
+    
+// }
+// loadDate()
+
+// let display=  (p)  =>{
+//     p.forEach(h =>{
+//         console.log(h.brand);
+//     })
+
+// }
