12f460a2a00cb08ce9aa2acd1c9fa2fad808ed0b
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-03-06 07:50:41 +0600

    Billonaire person added and final worth display in UI

--- Billonaire-Dairy/js/getAllBillonaire.js
+++ Billonaire-Dairy/js/getAllBillonaire.js
@@ -12,26 +12,38 @@ const fetchDataBiilonair = async () => {
 // Fetch AllBilonair data form server {End} 
 
 
-const displayAllBillonair = (data) => {
+const displayAllBillonair = (data , dataLimit = 10) => {
+    data = data.splice(0, dataLimit)
 
+    //! Sum of all Worth and show in "totlaWealth" id [Start]
+    let sumOfFinalWorth = 0;
+    //! Sum of all Worth and show in "totlaWealth" id [End]
 
-    data = data.splice(0, 5)
     data.forEach(item => {
         const { personName, countryOfCitizenship, industries, rank, finalWorth } = item
         let tableRowContainer = document.getElementById('table-row-container');
         let tableRow = document.createElement('tr');
         tableRow.classList.add('text-xl', 'font-medium')
         tableRow.innerHTML += `
-       <td>${personName}</td>
+       <td>${personName} <button onclick="modalOpenInEyeIcon()"><img src="./logo/eyeIcon.png" alt="Eye Icon" class="inline ml-1"></button></td>
        <td>${countryOfCitizenship}</td>
-       <td>${ industries[0]}</td>
+       <td>${industries[0]}</td>
        <td>${rank}</td>
        <td>${finalWorth}</td> 
-       `
-       tableRowContainer.append(tableRow);
+     `
+        tableRowContainer.append(tableRow);
+        return sumOfFinalWorth += finalWorth;
     })
-
+    //! Sum of all Worth and show in "totlaWealth" id [Start] 
+    document.getElementById('totlaWealth').innerText = sumOfFinalWorth;
+    //! Sum of all Worth and show in "totlaWealth" id [End]s
 }
+
+// This function for Modal Starts 
+const modalOpenInEyeIcon=()=>{
+    console.log('Hello  Nayemn');
+}
+// This function for Modal Ends
 
 
 fetchDataBiilonair()
